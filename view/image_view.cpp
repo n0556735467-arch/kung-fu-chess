4fe1dd2b808dc7010c96b5640188a4798f533eed
Nechami <n0556735467@gmail.com>
2026-07-28 13:26:30 +0300

    Fix ImageView::startState: use insert_or_assign instead of operator[] (PerPieceAnim has no default constructor)

---
+++ view/image_view.cpp
@@ -0,0 +1,38 @@
+#include "image_view.hpp"
+
+ImageView::ImageView(const AnimationConfigProvider& configProvider)
+    : configProvider(configProvider) {}
+
+void ImageView::startState(int pieceId, const std::string& stateName) {
+    AnimationConfig cfg = configProvider.configFor(stateName);
+    animations.insert_or_assign(pieceId, PerPieceAnim{stateName, AnimationClock(cfg)});
+}
+
+void ImageView::update(int pieceId, const std::string& triggeredState, int ms) {
+    bool isForceTrigger = (triggeredState == "move" || triggeredState == "jump");
+
+    auto it = animations.find(pieceId);
+    if (it == animations.end()) {
+        startState(pieceId, triggeredState);
+    } else if (isForceTrigger && it->second.stateName != triggeredState) {
+        startState(pieceId, triggeredState);
+    }
+
+    it = animations.find(pieceId);
+    it->second.clock.advance(ms);
+
+    if (it->second.clock.isFinished()) {
+        AnimationConfig cfg = configProvider.configFor(it->second.stateName);
+        if (!cfg.nextStateWhenFinished.empty()) {
+            startState(pieceId, cfg.nextStateWhenFinished);
+        }
+    }
+}
+
+std::string ImageView::currentStateName(int pieceId) const {
+    return animations.at(pieceId).stateName;
+}
+
+int ImageView::currentFrame(int pieceId) const {
+    return animations.at(pieceId).clock.currentFrame();
+}
